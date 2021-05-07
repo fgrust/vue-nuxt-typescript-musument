33e2948549f6a8fac37cd71257e914d17482e3a1
Ho Ho <aliklabe.buz@gmail.com>
2021-05-06 21:59:11 -0400

    feat: update card with vuex

--- @types/index.d.ts
+++ @types/index.d.ts
@@ -3,17 +3,29 @@ export interface IProductItem {
   title: string
   description: string
   discount: number
-  retailPrice: IRetailPrice
-  originalRetailPrice: IRetailPrice
-  coverImageUrl: string
+  retail_price: IRetailPrice
+  original_retail_price: IRetailPrice
+  cover_image_url: string
 }
 
 export interface IRetailPrice {
   value: number
-  formattedValue: string
+  formatted_value: string
 }
 
 export interface IFetchParam {
   limit: number
   offset: number
+}
+
+export interface IBagItem {
+  uuid: string
+  img: string
+  title: string
+  price: number
+}
+
+export interface IState {
+  bag: Array<IBagItem>
+  wishlist: Array<string>
 }
---
+++ components/Card.vue
@@ -0,0 +1,101 @@
+<template>
+  <b-card
+    :img-src="`${product.cover_image_url}&q=60&fit=crop&h=320`"
+    :img-alt="product.title"
+    img-top
+    :title="product.title"
+    class="mb-3"
+  >
+    <b-card-text v-html="product.description" />
+    <b-card-text class="text-center price">
+      <span
+        v-if="isDiscount"
+        class="stroke-text"
+        v-html="product.original_retail_price.formatted_value"
+      />
+      <span
+        :class="{ 'text-danger': isDiscount }"
+        v-html="product.retail_price.formatted_value"
+      />
+    </b-card-text>
+    <favorite-button
+      class="btn-favorite"
+      :isActive="isinw"
+      @click="toggleFavorite"
+    />
+
+    <b-button
+      pill
+      variant="outline-secondary"
+      block
+      :disabled="isinb"
+      @click="addToCart"
+      >{{ isInBag ? 'IN CART' : 'ADD TO CART' }}</b-button
+    >
+  </b-card>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop, Getter, Mutation } from 'nuxt-property-decorator'
+import { IProductItem } from '~/@types'
+import FavoriteButton from './FavoriteButton.vue'
+
+@Component({ components: { FavoriteButton } })
+export default class Card extends Vue {
+  @Prop() product?: IProductItem
+
+  @Getter isInWishlist!: Function
+  @Getter isInBag!: Function
+
+  @Mutation addToBag!: Function
+  @Mutation addToWishlist!: Function
+  @Mutation removeFromWishlist!: Function
+
+  get isinw() {
+    return this.product && this.isInWishlist(this.product.uuid)
+  }
+
+  get isinb() {
+    return this.product && this.isInBag(this.product.uuid)
+  }
+
+  get isDiscount() {
+    return this.product && this.product.discount > 0
+  }
+
+  toggleFavorite() {
+    if (this.product) {
+      this.isinw
+        ? this.removeFromWishlist(this.product.uuid)
+        : this.addToWishlist(this.product.uuid)
+    }
+  }
+
+  addToCart() {
+    this.product &&
+      this.addToBag({
+        uuid: this.product.uuid,
+        price: this.product.retail_price.value,
+        title: this.product.title,
+        img: this.product.cover_image_url,
+      })
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.stroke-text {
+  text-decoration: line-through;
+}
+.btn-favorite {
+  position: absolute;
+  top: 8px;
+  right: 8px;
+}
+button.disabled,
+button:disabled {
+  background-color: #c8d7e6;
+  color: #525a61;
+  font-weight: 600;
+}
+</style>
